34737b9d550fdd1fde73eceae7c212ab7e8a1277
Vitalii Zakharchuk <95skin95@gmail.com>
2026-04-27 16:38:45 -0300

    fix error with id

--- src/core/middleware/validation/params-id-validation.middleware.ts
+++ src/core/middleware/validation/params-id-validation.middleware.ts
@@ -5,6 +5,4 @@ export const paramsIdValidationMiddleware = param("id")
   .withMessage("Id is required")
   .isString()
   .withMessage("Id must be a string")
-  .trim()
-  .isLength({ min: 1, max: 24 })
-  .withMessage("Id must be between 1 and 24 characters");
+  .trim();
