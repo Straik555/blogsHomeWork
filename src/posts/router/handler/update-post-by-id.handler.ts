1e21eb484e5fba8fe04d74026a607607c2c8e8e2
Vitalii Zakharchuk <95skin95@gmail.com>
2026-04-30 11:26:02 -0300

    fix created blog

--- src/posts/router/handler/update-post-by-id.handler.ts
+++ src/posts/router/handler/update-post-by-id.handler.ts
@@ -9,16 +9,21 @@ import { createErrorMessage } from "../../../core/middleware/validation/input-va
 import { errorMessage } from "../../../core/utils/errorMessage.utils";
 import { postsRepository } from "../../repositories/posts.repository";
 import { blogsRepository } from "../../../blogs/repositories/blogs.repository";
+import { WithId } from "mongodb";
+import { BlogsType } from "../../../blogs/types/blogs.type";
 
 export const updatePostByIdHandler = async (
   req: RequestWithParamsAndBody<UriParamsById, PostViewModel>,
   res: Response<PostViewModel | ErrorsResponse>,
 ) => {
+  console.log("req.body", req.body);
   try {
     const { id } = req.params;
     const { body } = req;
 
-    const foundBlog = await blogsRepository.getById(body.blogId);
+    const foundBlog: WithId<BlogsType> | null = await blogsRepository.getById(
+      body.blogId,
+    );
 
     if (!foundBlog) {
       res.status(HTTP_STATUS.NOT_FOUND_404).send(
@@ -31,16 +36,21 @@ export const updatePostByIdHandler = async (
       );
       return;
     }
-
     const newPost: PostsType = {
       ...body,
       blogName: foundBlog.name,
     };
 
+    const aaaa = await postsRepository.getById(id);
+    console.log("aaaa", aaaa);
+
     await postsRepository.update(id, newPost);
 
     res.sendStatus(HTTP_STATUS.NO_CONTENT_204);
   } catch (error) {
+    if (error === "Post not found") {
+      res.sendStatus(HTTP_STATUS.NOT_FOUND_404);
+    }
     res.sendStatus(HTTP_STATUS.INTERNAL_SERVER_ERROR_500);
   }
 };
